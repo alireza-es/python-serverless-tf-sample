80fef984680056bdddeefca542ed40863a402f31
Alireza Esmaeili <alireza.es@gmail.com>
2024-06-22 18:12:57 -0600

    update terraform

--- terraform/api_gateway/api_gateway.tf
+++ terraform/api_gateway/api_gateway.tf
@@ -1,68 +1,37 @@
-resource "aws_api_gateway_rest_api" "api" {
-  name        = var.api_name
-  description = var.description
+variable "lambda_function_arns" {
+  type = map(string)
 }
 
-resource "aws_api_gateway_resource" "resource_one" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "endpoint_one"
+resource "aws_api_gateway_rest_api" "this" {
+  name = "MyAPIGateway"
 }
 
-resource "aws_api_gateway_resource" "resource_two" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "endpoint_two"
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "method_one" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource_one.id
-  http_method   = "GET"
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "method_two" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource_two.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_lambda_permission" "api_gw_one" {
-  statement_id  = "AllowAPIGatewayInvokeOne"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_functions["lambda_function_one"]
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/GET/endpoint_one"
-}
-
-resource "aws_lambda_permission" "api_gw_two" {
-  statement_id  = "AllowAPIGatewayInvokeTwo"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_functions["lambda_function_two"]
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/GET/endpoint_two"
-}
-
-resource "aws_api_gateway_integration" "integration_one" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource_one.id
-  http_method             = aws_api_gateway_method.method_one.http_method
+resource "aws_api_gateway_integration" "proxy" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.lambda_functions["lambda_function_one"]
+  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_function_arns["lambda_function_one"]}/invocations"
 }
 
-resource "aws_api_gateway_integration" "integration_two" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource_two.id
-  http_method             = aws_api_gateway_method.method_two.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = var.lambda_functions["lambda_function_two"]
-}
-
-output "api_gateway_url" {
-  value = aws_api_gateway_deployment.api.invoke_url
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_arns["lambda_function_one"]
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.this.id}/*/*/{proxy+}"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,29 @@
-provider "aws" {
-  region = var.region
-}
-
 module "lambda_function_one" {
-  source = "./lambda"
+  source       = "./lambda_function"
   function_name = "lambda_function_one"
-  file_path = "${path.module}/../src/lambda_function_payload/lambda_function_one_payload.zip"
-  handler = "lambda_function_one.lambda_handler"
-  runtime = "python3.9"
-  description = "Lambda function for endpoint one"
+  handler       = "lambda_function_one.lambda_handler"
+  runtime       = "python3.9"
+  filename      = "path/to/lambda_function_one.zip"
+  environment_variables = {
+    VAR_NAME = "value"
+  }
 }
 
 module "lambda_function_two" {
-  source = "./lambda"
+  source       = "./lambda_function"
   function_name = "lambda_function_two"
-  file_path = "${path.module}/../src/lambda_function_payload/lambda_function_two_payload.zip"
-  handler = "lambda_function_two.lambda_handler"
-  runtime = "python3.9"
-  description = "Lambda function for endpoint two"
+  handler       = "lambda_function_two.lambda_handler"
+  runtime       = "python3.9"
+  filename      = "path/to/lambda_function_two.zip"
+  environment_variables = {
+    VAR_NAME = "value"
+  }
 }
 
 module "api_gateway" {
-  source = "./api_gateway"
-  api_name = "MyAPIGateway"
-  description = "API Gateway for my Lambda functions"
-  lambda_functions = {
-    "lambda_function_one" = module.lambda_function_one.lambda_function_arn
-    "lambda_function_two" = module.lambda_function_two.lambda_function_arn
+  source  = "./api_gateway"
+  lambda_function_arns = {
+    lambda_function_one = module.lambda_function_one.lambda_function_arn
+    lambda_function_two = module.lambda_function_two.lambda_function_arn
   }
 }
